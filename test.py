228fd4f9195c0f92df133bac9d94ae343971df50
Vlad Klimov <vladk@degirum.com>
2025-12-15 18:42:30 -0800

    Repo repair

--- test.py
+++ test.py
@@ -15,95 +15,143 @@ import time
 from src.anti_spoof_predict import AntiSpoofPredict
 from src.generate_patches import CropImage
 from src.utility import parse_model_name
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 
-SAMPLE_IMAGE_PATH = "./images/sample/"
-
-
-# 因为安卓端APK获取的视频流宽高比为3:4,为了与之一致，所以将宽高比限制为3:4
-def check_image(image):
-    height, width, channel = image.shape
-    if width/height != 3/4:
-        print("Image is not appropriate!!!\nHeight/Width should be 4/3.")
-        return False
-    else:
-        return True
-
-
-def test(image_name, model_dir, device_id):
+def test_camera(model_dir, device_id, camera_id=0):
+    """
+    Test anti-spoofing on camera stream
+    Args:
+        model_dir: path to anti-spoof models
+        device_id: GPU device id
+        camera_id: camera device id (default 0)
+    """
     model_test = AntiSpoofPredict(device_id)
     image_cropper = CropImage()
-    image = cv2.imread(SAMPLE_IMAGE_PATH + image_name)
-    result = check_image(image)
-    if result is False:
+
+    # Open camera
+    cap = cv2.VideoCapture(camera_id)
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
         return
-    image_bbox = model_test.get_bbox(image)
-    prediction = np.zeros((1, 3))
-    test_speed = 0
-    # sum the prediction from single model's result
-    for model_name in os.listdir(model_dir):
-        h_input, w_input, model_type, scale = parse_model_name(model_name)
-        param = {
-            "org_img": image,
-            "bbox": image_bbox,
-            "scale": scale,
-            "out_w": w_input,
-            "out_h": h_input,
-            "crop": True,
-        }
-        if scale is None:
-            param["crop"] = False
-        img = image_cropper.crop(**param)
-        start = time.time()
-        prediction += model_test.predict(img, os.path.join(model_dir, model_name))
-        test_speed += time.time()-start
 
-    # draw result of prediction
-    label = np.argmax(prediction)
-    value = prediction[0][label]/2
-    if label == 1:
-        print("Image '{}' is Real Face. Score: {:.2f}.".format(image_name, value))
-        result_text = "RealFace Score: {:.2f}".format(value)
-        color = (255, 0, 0)
-    else:
-        print("Image '{}' is Fake Face. Score: {:.2f}.".format(image_name, value))
-        result_text = "FakeFace Score: {:.2f}".format(value)
-        color = (0, 0, 255)
-    print("Prediction cost {:.2f} s".format(test_speed))
-    cv2.rectangle(
-        image,
-        (image_bbox[0], image_bbox[1]),
-        (image_bbox[0] + image_bbox[2], image_bbox[1] + image_bbox[3]),
-        color, 2)
-    cv2.putText(
-        image,
-        result_text,
-        (image_bbox[0], image_bbox[1] - 5),
-        cv2.FONT_HERSHEY_COMPLEX, 0.5*image.shape[0]/1024, color)
+    print("Press 'q' to quit")
 
-    format_ = os.path.splitext(image_name)[-1]
-    result_image_name = image_name.replace(format_, "_result" + format_)
-    cv2.imwrite(SAMPLE_IMAGE_PATH + result_image_name, image)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture frame.")
+            break
+
+        # Get face bounding box
+        image_bbox = model_test.get_bbox(frame)
+
+        if image_bbox[0] == -1:
+            # No face detected
+            cv2.putText(
+                frame,
+                "No face detected",
+                (10, 30),
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.7,
+                (0, 0, 255),
+                2,
+            )
+        else:
+            # Perform prediction
+            prediction = np.zeros((1, 3))
+            test_speed = 0
+
+            # Sum predictions from all models
+            for model_name in os.listdir(model_dir):
+                h_input, w_input, model_type, scale = parse_model_name(model_name)
+                param = {
+                    "org_img": frame,
+                    "bbox": image_bbox,
+                    "scale": scale,
+                    "out_w": w_input,
+                    "out_h": h_input,
+                    "crop": True,
+                }
+                if scale is None:
+                    param["crop"] = False
+                img = image_cropper.crop(**param)
+                start = time.time()
+                prediction += model_test.predict(
+                    img, os.path.join(model_dir, model_name)
+                )
+                test_speed += time.time() - start
+
+            # Draw result
+            label = np.argmax(prediction)
+            value = prediction[0][label] / 2
+
+            if label == 1:
+                result_text = "RealFace: {:.2f}".format(value)
+                color = (0, 255, 0)  # Green for real
+            else:
+                result_text = "FakeFace: {:.2f}".format(value)
+                color = (0, 0, 255)  # Red for fake
+
+            # Draw bounding box
+            cv2.rectangle(
+                frame,
+                (image_bbox[0], image_bbox[1]),
+                (image_bbox[0] + image_bbox[2], image_bbox[1] + image_bbox[3]),
+                color,
+                2,
+            )
+
+            # Draw prediction text
+            cv2.putText(
+                frame,
+                result_text,
+                (image_bbox[0], image_bbox[1] - 10),
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.7,
+                color,
+                2,
+            )
+
+            # Draw FPS
+            fps_text = "FPS: {:.2f}".format(1.0 / test_speed if test_speed > 0 else 0)
+            cv2.putText(
+                frame,
+                fps_text,
+                (10, 30),
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.7,
+                (255, 255, 255),
+                2,
+            )
+
+        # Display frame
+        cv2.imshow("Anti-Spoofing Detection", frame)
+
+        # Exit on 'q' key
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    # Release resources
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    desc = "test"
+    desc = "Anti-spoofing test on camera stream"
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument(
-        "--device_id",
-        type=int,
-        default=0,
-        help="which gpu id, [0/1/2/3]")
+        "--device_id", type=int, default=0, help="which gpu id, [0/1/2/3]"
+    )
     parser.add_argument(
         "--model_dir",
         type=str,
         default="./resources/anti_spoof_models",
-        help="model_lib used to test")
+        help="model_lib used to test",
+    )
     parser.add_argument(
-        "--image_name",
-        type=str,
-        default="image_F1.jpg",
-        help="image used to test")
+        "--camera_id", type=int, default=0, help="camera device id (default 0)"
+    )
     args = parser.parse_args()
-    test(args.image_name, args.model_dir, args.device_id)
+    test_camera(args.model_dir, args.device_id, args.camera_id)
